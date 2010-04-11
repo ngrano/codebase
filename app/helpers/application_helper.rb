4d884e83918e81371eb2e0414f1380584148f35a
Simo Niemelä <simo.niemela@gmail.com>
2010-04-11 22:59:22 +0300

    Set a current class to a link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def current_class_if(*args)
+    options = args.extract_options!
+    
+    if parts = args.first
+      controller, action = parts.split('#')
+      options.merge!(:controller => controller, :action => action)
+    end
+    
+    'current' if current_page?(options)
+  end
 end
