049cdacc376cd0c9d2bed42ce70ce0ca9fff69f0
Simo Niemelä <simo.niemela@gmail.com>
2010-04-13 00:11:05 +0300

    Add a link to show current user's profile

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,15 @@
 class Admin::UsersController < ApplicationController
+  respond_to :html
+
   before_filter :authenticate_user!
 
   layout 'admin'
 
   def index
-    @users = User.order('name')
+    respond_with(@users = User.order('name'))
+  end
+
+  def show
+    respond_with(@user = User.find(params[:id]))
   end
 end
