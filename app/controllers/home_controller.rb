9299ceb3b4b0d29679a4b6c93a958b7efd79468d
Simo Niemelä <simo.niemela@gmail.com>
2010-04-16 21:09:10 +0300

    Add utf-8 encoding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class HomeController < ApplicationController
   def index
     # views/posts/index.html.erb
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class PostsController < ApplicationController
 
 def index 
