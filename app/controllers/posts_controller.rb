9fedb4ae41225d6ea3b7c91cc9e392395330b587
Niklas Granö <niklas.grano@gmail.com>
2010-04-16 18:29:40 +0300

    Add listing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+def index 
+  @posts = Post.all
+ 
+  respond_to do |format|
+    format.html # index.html.erb  
+    format.xml { render :xml => @posts } 
+  end 
+end 
 end
