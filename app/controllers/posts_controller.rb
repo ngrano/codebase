9668aa709aafa0579cc0a09e26e26b813b379d05
Simo Niemelä <simo.niemela@gmail.com>
2010-04-17 14:29:26 +0300

    Fix code indentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,48 @@
 # encoding: utf-8
 
 class PostsController < ApplicationController
-def new
-  @post = Post.new
+  def new
+    @post = Post.new
 
-  respond_to do |format|
-    format.html # new.html.erb
-    format.xml  {render :xml => @post }
-  end
-end
-
-def create
-  @post = Post.new(params[:post])
-
-  respond_to do |format|
-    if @post.save
-      format.html { redirect_to(@post,
-                    :notice => 'Artikkeli lisätty onnistuneesti') }
-      format.xml  { render :xml =>  @post,
-                    :status => :created, :location => @post }
-    else
-      format.html { render :action => "new"  }
-      format.xml  { render :xml => @post.errors,
-                    :status => :unprocessable_entity  }
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  {render :xml => @post }
     end
   end
-end
 
-def show
-  @post = Post.find(params[:id])  
-    
-  respond_to do |format| 
-    format.html # show.html.erb  
-    format.xml { render :xml => @post } 
-   end 
-end
+  def create
+    @post = Post.new(params[:post])
 
-def destroy 
-  @post = Post.find(params[:id])  
-  @post.destroy 
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to(@post,
+                      :notice => 'Artikkeli lisätty onnistuneesti') }
+        format.xml  { render :xml =>  @post,
+                      :status => :created, :location => @post }
+      else
+        format.html { render :action => "new"  }
+        format.xml  { render :xml => @post.errors,
+                      :status => :unprocessable_entity  }
+      end
+    end
+  end
 
-  respond_to do |format| 
-    format.html { redirect_to(posts_url) } 
-    format.xml { head :ok } 
-  end 
-end 
+  def show
+    @post = Post.find(params[:id])  
 
+    respond_to do |format| 
+      format.html # show.html.erb  
+      format.xml { render :xml => @post } 
+     end 
+  end
+
+  def destroy
+    @post = Post.find(params[:id])  
+    @post.destroy 
+
+    respond_to do |format| 
+      format.html { redirect_to(posts_url) } 
+      format.xml { head :ok } 
+    end 
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base 
-validates :body,  :presence => true 
-validates :title, :presence => true,  
-                  :length => { :minimum => 5 } 
+  validates :body,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 } 
 end 
