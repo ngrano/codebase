fdb71002a9ef1c026cb036b296f242ff4a27cc30
Niklas Granö <niklas.grano@gmail.com>
2010-04-17 19:34:08 +0300

    Add comment-module

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100412130142) do
+ActiveRecord::Schema.define(:version => 20100417162935) do
+
+  create_table "blogs", :force => true do |t|
+    t.string   "name"
+    t.string   "alias"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", :force => true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
