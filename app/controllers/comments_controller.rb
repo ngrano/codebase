ce4238089c81f68c9980a986aa563cce698151e9
Niklas Granö <niklas.grano@gmail.com>
2010-04-20 20:44:53 +0300

    Add some methods in comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create 
+    @post = Post.find(params[:post_id])  
+    @comment = @post.comments.create(params[:comment])  
+    redirect_to post_path(@post)  
+  end 
+
+  def destroy 
+    @post = Post.find(params[:post_id])  
+    @comment = @post.comments.find(params[:id])  
+    @comment.destroy redirect_to post_path(@post)  
+  end
+
 end
