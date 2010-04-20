279ac414d173688b0b156dbb6951b3b625fb2328
Niklas Granö <niklas.grano@gmail.com>
2010-04-20 19:22:53 +0300

    Modify delete the comments associated with post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   validates :body,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 } 
+
+  has_many :comments, :dependent => :destroy
 end 
