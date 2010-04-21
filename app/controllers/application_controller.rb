7a4a31784de5f2792b42027379e91fb31a9087b2
Simo Niemelä <simo.niemela@gmail.com>
2010-04-22 01:14:37 +0300

    Move devise translations into fi.yml and use Finnish as the default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  protected
+    def set_locale
+      I18n.locale = 'fi'
+    end
 end
