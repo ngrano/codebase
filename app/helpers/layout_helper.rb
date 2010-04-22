97deeed125ab95dc12c94e4da00d4ef76a47f50d
Simo Niemelä <simo.niemela@gmail.com>
2010-04-22 15:14:54 +0300

    Add a link_to_back helper

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,16 +1,24 @@
 module LayoutHelper
   def or_cancel(options = {})
     text = options[:text] || t('cancel')
-    or_text = options[:or] || t('or') 
+    or_text = options[:or] || t('or')
+    link = link_to_back(text, options)
+    "#{or_text} #{link}".html_safe
+  end
 
-    link = if to = options[:to]
+  def or_back(options = {})
+    or_cancel(options.reverse_merge!(:text => t('back')))
+  end
+  
+  def link_to_back(text = nil, options = {})
+    text = text || t('back')
+    
+    if to = options[:to]
       link_to text, to
     elsif referer = request.headers["Referer"]
       link_to text, referer
     else
       link_to text, root_path
     end
-    
-    "#{or_text} #{link}".html_safe
   end
 end
