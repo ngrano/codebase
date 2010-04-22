6fafd0b9ec49fffb0ef905a4dc583e6798bf7e65
Simo Niemelä <simo.niemela@gmail.com>
2010-04-22 16:17:18 +0300

    Add a markup column to the post table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100417162935) do
+ActiveRecord::Schema.define(:version => 20100422131338) do
 
   create_table "blogs", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20100417162935) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "markup"
   end
 
   create_table "users", :force => true do |t|
