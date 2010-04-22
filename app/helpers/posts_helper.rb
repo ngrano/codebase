d39cd16c87ca009d7f2798b767639acf8623b9a5
Simo Niemelä <simo.niemela@gmail.com>
2010-04-22 18:21:23 +0300

    Add a markup helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def markup(body, markup_lang)
+    case markup_lang.to_s.downcase.to_sym
+    when :markdown
+      RDiscount.new(body).to_html
+    when :textile
+      RedCloth.new(body).to_html
+    else
+      body
+    end.html_safe
+  end
 end
