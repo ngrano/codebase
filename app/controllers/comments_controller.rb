d877ec4d847842d5c657ea73ffc1160d3d9eadb4
Simo Niemelä <simo.niemela@gmail.com>
2010-04-22 19:18:08 +0300

    Make sure that commenter and body is not empty and respond with a comment object

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,22 @@
 class CommentsController < ApplicationController
   before_filter :find_post
 
+  respond_to :html
+
   def new
     @comment = @post.comments.build
   end
 
   def create 
-    @comment = @post.comments.create(params[:comment])
-    redirect_to @post
+    @comment = @post.comments.build(params[:comment])
+    flash[:notice] = 'Kommentti luotiin onnistuneesti' if @comment.save
+    respond_with(@comment, :location => @post)
   end 
 
   def destroy 
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to @post
+    respond_with(@comment, :location => @post)
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates_presence_of :commenter, :body
+
   belongs_to :post
 end
