821e7a74536752940ec4de504b36b0a64e4bbdd7
Simo Niemelä <simo.niemela@gmail.com>
2010-04-28 22:09:30 +0300

    Install tipsy

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  $('a[rel=tipsy], img[rel=tipsy]').tipsy({gravity: 's'});
+});
