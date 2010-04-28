4f14d32de70ae6e82845df460fade2d2f315fd2f
Simo Niemelä <simo.niemela@gmail.com>
2010-04-28 23:46:30 +0300

    Bind a hide event on flash message

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,8 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
   $('a[rel=tipsy], img[rel=tipsy]').tipsy({gravity: 's'});
+
+  $('.hide_flash').click(function(){
+    $(this).parent().hide('fast');
+  });
 });
