e4953f22a656d75901e758a4dc3fab3e7da7d1a6
Simo Niemelä <simo.niemela@gmail.com>
2010-04-29 00:59:52 +0300

    Fix whitespace

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
-	class Post < ActiveRecord::Base
+class Post < ActiveRecord::Base
   MARKUP_LANGUAGES = ['Markdown', 'Textile', 'HTML']
-  
+
   default_scope order('created_at DESC')
-  
+
   validates :body,  :presence => true
   validates :markup, :presence => true
   validates :blog_id, :presence => true
-  
+
   validates :title, :presence => true,
                     :uniqueness => true,
                     :length => { :minimum => 5 }
@@ -14,4 +14,4 @@
   belongs_to :blog
   belongs_to :user
   has_many :comments, :dependent => :destroy
-end 
+end
