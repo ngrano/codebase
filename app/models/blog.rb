8603beb3e854fb0594821a8935e8d8c9796e4bc9
Linkkeri <Linkkeri@gmail.com>
2010-04-29 00:48:22 +0300

    cleaned up empty rows

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,4 @@
 class Blog < ActiveRecord::Base
-
-
-
   validates_presence_of :name, :alias
-  has_many :posts, :order => "posted on", :dependent => :destroy
+  has_many :posts, :order => "created_at DESC", :dependent => :destroy
 end
