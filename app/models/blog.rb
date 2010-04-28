99baff4bcd671c9e447c6cd632b20d28b4ece903
Linkkeri <Linkkeri@gmail.com>
2010-04-29 00:37:32 +0300

    fix order on posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,7 @@
 class Blog < ActiveRecord::Base
+
+
+
   validates_presence_of :name, :alias
-  has_many :posts, :dependent => :destroy
+  has_many :posts, :order => "posted on", :dependent => :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ActiveRecord::Base
+	class Post < ActiveRecord::Base
   MARKUP_LANGUAGES = ['Markdown', 'Textile', 'HTML']
   
   default_scope order('created_at DESC')
