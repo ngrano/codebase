77b31b3be815141e302d83aedab901e6e56f72c5
Simo Niemelä <simo.niemela@gmail.com>
2011-01-08 16:37:49 +0200

    Use sha1 encryptor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable,
+      :encryptable, :encryptor => :sha1
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :nick, :email, :password, :password_confirmation, :remember_me
