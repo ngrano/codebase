58a24de7948f7b12f7f1c5bce0ee3d9015a12e74
Simo Niemelä <simo.niemela@gmail.com>
2011-01-10 14:12:38 +0200

    Remove encryptable and password_salt

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable,
-      :encryptable, :encryptor => :sha1
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :nick, :email, :password, :password_confirmation, :remember_me
